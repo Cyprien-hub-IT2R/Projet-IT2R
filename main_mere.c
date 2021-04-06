3366bf48a44833d217c765341128494ec7a16e26
Sylvain <80481070+Sylvain512@users.noreply.github.com>
2021-04-06 15:30:03 +0200

    Add files via upload

--- main_mere.c
+++ main_mere.c
@@ -1,11 +1,3 @@
-/*---------------------------------------------------
-* CAN 2 uniquement en TX 
-* + réception CAN1 
-* avec RTOS et utilisation des fonction CB
-* pour test sur 1 carte -> relier CAN1 et CAN2
-* 2017-04-02 - XM
----------------------------------------------------*/
-
 #define osObjectsPublic                     // define objects in main module
 #include "osObjects.h"                      // RTOS object definitions
 
@@ -27,53 +19,50 @@ extern GLCD_FONT GLCD_Font_6x8;
 extern GLCD_FONT GLCD_Font_16x24;
 
 typedef struct{
-		char  donnee1;
-		char  donnee2;
-		char  donnee3;
-		char  donnee4;
+		char data_ultrason;
+		char data_phares;
+		char data_gps;
 	}MaStruct;
 
 extern   ARM_DRIVER_CAN         Driver_CAN1;
 extern   ARM_DRIVER_CAN         Driver_CAN2;
 
 osMailQId ID_BAL_ultrason ;
-osMailQDef (NOM_BAL, 50,MaStruct) ;
+osMailQDef (NOM_BAL_ultrason, 20,MaStruct) ;
 	
 osMailQId ID_BAL_phares ;
-osMailQDef (NOM_BAL_phares, 50,MaStruct) ;
+osMailQDef (NOM_BAL_phares, 20,MaStruct) ;
 
 osMailQId ID_BAL_gps ;
-osMailQDef (NOM_BAL_gps, 50,MaStruct) ;	
-	
-	
+osMailQDef (NOM_BAL_gps, 20,MaStruct) ;	
 
 // CAN1 utilisé pour réception
 void myCAN1_callback(uint32_t obj_idx, uint32_t event)
 {
-    switch (event)
-    {
+	switch (event)
+  {
     case ARM_CAN_EVENT_RECEIVE:
-        /*  Message was received successfully by the obj_idx object. */
-       osSignalSet(id_CANthreadR, 0x01);
-        break;
-    }
+			/*  Message was received successfully by the obj_idx object. */
+			osSignalSet(id_CANthreadR, 0x01);
+		break;
+	}
 }
 
 // CAN2 utilisé pour émission
 void myCAN2_callback(uint32_t obj_idx, uint32_t event)
 {
-    switch (event)
-    {
-    case ARM_CAN_EVENT_SEND_COMPLETE:
-        /* 	Message was sent successfully by the obj_idx object.  */
-        osSignalSet(id_CANthreadT, 0x02);
-        break;
-    }
+	switch (event)
+	{
+		case ARM_CAN_EVENT_SEND_COMPLETE:
+		/* 	Message was sent successfully by the obj_idx object.  */
+		osSignalSet(id_CANthreadT, 0x02);
+		break;
+	}
 }
 
 // CAN1 utilisé pour réception
 void InitCan1 (void) 
-	{
+{
 	Driver_CAN1.Initialize(NULL,myCAN1_callback);
 	Driver_CAN1.PowerControl(ARM_POWER_FULL);
 	
@@ -86,14 +75,13 @@ void InitCan1 (void)
                           ARM_CAN_BIT_SJW(1U));                // Resynchronization jump width is same as phase segment 2
                           
 	// Mettre ici les filtres ID de réception sur objet 0
-	//....................................................
 		
 	Driver_CAN1.ObjectConfigure(0,ARM_CAN_OBJ_RX);				// Objet 0 du CAN1 pour réception
 	
-	Driver_CAN1.ObjectSetFilter(0, ARM_CAN_FILTER_ID_EXACT_ADD,ARM_CAN_STANDARD_ID(0x001),0) ; // Objet 0 : réception Status capteur Ultrason
-	Driver_CAN1.ObjectSetFilter(0, ARM_CAN_FILTER_ID_EXACT_ADD,ARM_CAN_STANDARD_ID(0x002),0) ; // Objet 0 : réception capteur luminosité + Leds
-	Driver_CAN1.ObjectSetFilter(0, ARM_CAN_FILTER_ID_EXACT_ADD,ARM_CAN_STANDARD_ID(0x004),0) ; // Objet 0 : réception GPS
-	
+	Driver_CAN1.ObjectSetFilter(0, ARM_CAN_FILTER_ID_EXACT_ADD,ARM_CAN_STANDARD_ID(0x002),0) ; // Objet 0 : réception et ID=0x002 ultra son
+	Driver_CAN1.ObjectSetFilter(0, ARM_CAN_FILTER_ID_EXACT_ADD,ARM_CAN_STANDARD_ID(0x003),0) ; // Objet 0 : réception et ID=0x003 phares
+	Driver_CAN1.ObjectSetFilter(0, ARM_CAN_FILTER_ID_EXACT_ADD,ARM_CAN_STANDARD_ID(0x004),0) ; // Objet 0 : réception et ID=0x004 gps
+
 	Driver_CAN1.SetMode(ARM_CAN_MODE_NORMAL);					// fin init
 }
 
@@ -120,18 +108,18 @@ void InitCan2 (void)
 }
 
 
-// tache envoi toutes les secondes
+// Thread envoi
 void CANthreadT(void const *argument)
 {
+	ARM_CAN_MSG_INFO tx_msg_info;
+	
 	char identifiant,retour;
 	char texte1[10];
 	char texte2[10];
 	uint8_t data_buf[8];
-	ARM_CAN_MSG_INFO tx_msg_info;
-	
 
-	while (1) {
-		
+	while (1)
+	{
 		osSignalWait(0x02, osWaitForever);		// sommeil en attente fin emission
 		
 		tx_msg_info.id = ARM_CAN_STANDARD_ID (0x001);
@@ -141,100 +129,89 @@ void CANthreadT(void const *argument)
 		retour = data_buf [0] ; // 1ère donnée de la trame récupérée (char)
 		Driver_CAN2.MessageSend(1, &tx_msg_info, data_buf, 1); // 1 data à envoyer	
 
+		osDelay(50);
+	}		
+}
+
+// Thread reception
+void CANthreadR(void const *argument)
+{
+	ARM_CAN_MSG_INFO  rx_msg_info;
+	uint8_t data_buf[8];
+	MaStruct *ptr;
+	char taille,identifiant,retour;
+	char texte1[20];
+	char texte2[20];
+	
+	while(1)
+	{
+		osSignalWait(0x01, osWaitForever);		// sommeil en attente réception
+		
+		Driver_CAN1.MessageRead(0, &rx_msg_info, data_buf, 8); // 8 data max
+		
+		identifiant = rx_msg_info.id;
+		taille = rx_msg_info.dlc; // nb data (char)
+		retour = data_buf[0];
+		
 //		sprintf(texte1,"ID= 0x%X",identifiant);
 //		GLCD_DrawString(1,1,(unsigned char*)texte1);
 //		sprintf(texte2,"DATA= 0x%X",retour);
 //		GLCD_DrawString(1,30,(unsigned char*)texte2);
 		
-		
-		osDelay(50);
-	}		
-}
-
-
-// tache reception
-void CANthreadR(void const *argument)
-{
-	ARM_CAN_MSG_INFO  rx_msg_info;
-	uint8_t data_buf[8];
-	char taille,ID;
-	char chaine[20];
+		switch(identifiant){
 	
-	while(1)
-	{	
-		
-		MaStruct*ptr;
-		
-		osSignalWait(0x01, osWaitForever);		// sommeil en attente réception
-	
-		Driver_CAN1.MessageRead(0, &rx_msg_info, data_buf, 8); // 8 data max
-		ID = rx_msg_info.id; // (int)
-		taille = rx_msg_info.dlc; // nb data (char)
-		
-		sprintf(chaine,"ID recu : %d",ID);
-		GLCD_DrawString(1,1,(unsigned char*)chaine);
-		
-//		switch(ID){
-//	
-//			case 0x002 :
-//				
-//			//stockage infrmation data capteur ultrason
-//			ptr = osMailAlloc(ID_BAL_ultrason, osWaitForever);
-//			ptr -> donnee1 = data_buf[0]; // valeur à envoyer
-//			osMailPut(ID_BAL_ultrason, ptr);
-//			
-//			// reveille tache à effectuer
-//			
-//				
-//			break;
-//			
-//			case 0x003 :
-//				
-//			// Reception  data status capteur luminosité + LEDS (phares)
-//			ptr = osMailAlloc(ID_BAL_phares, osWaitForever);
-//			ptr -> donnee2 = data_buf[0]; // valeur à envoyer
-//			osMailPut(ID_BAL_phares, ptr);
-//			
-//			// reveille tache à effectuer
-//			
-//				
-//			break;
-//			
-//			case 0x004:
-//				
-//			// Reception data GPS
-//			ptr = osMailAlloc(ID_BAL_gps, osWaitForever);
-//			ptr -> donnee3 = data_buf[0]; // valeur à envoyer
-//			osMailPut(ID_BAL_gps, ptr);
-//			
-//			// reveille tache à effectuer
-//			
-//			break;
-//		
-//		
-//	}
+			case 0x002 :
+
+				//stockage information data capteur ultrason
+				ptr = osMailAlloc(ID_BAL_ultrason, osWaitForever);
+				ptr -> data_ultrason = data_buf[0]; // valeur à envoyer
+				osMailPut(ID_BAL_ultrason, ptr);
+				
+				// reveille tache à effectuer
+				break;
+			
+			case 0x003 :
+				
+				// Reception  data status capteur luminosité + LEDS (phares)
+				ptr = osMailAlloc(ID_BAL_phares, osWaitForever);
+				ptr -> data_phares = data_buf[0]; // valeur à envoyer
+				osMailPut(ID_BAL_phares, ptr);
+				
+				// reveille tache à effectuer
+				break;
+			
+			case 0x004:
+				
+				// Reception data GPS
+				ptr = osMailAlloc(ID_BAL_gps, osWaitForever);
+				ptr -> data_gps = data_buf[0]; // valeur à envoyer
+				osMailPut(ID_BAL_gps, ptr);
+				
+				// reveille tache à effectuer
+				break;
+		}
 	}
 }
 
 void ultrason(void const *argument)
 {
-		osEvent result;
+		char chaine_ultrason[20];
 		MaStruct *recep;
 		MaStruct valeur_recue;
-		char chaine[20];
+		osEvent result;
 
 		while(1)
 		{
-		result = osMailGet(ID_BAL_ultrason, osWaitForever); // attente mail
-		recep = result.value.p; // on cible le pointeur...
-		valeur_recue = *recep ; // ...et la valeur pointée
-		osMailFree(ID_BAL_ultrason, recep); // libération mémoire allouée
-		
-		sprintf(chaine,"valeur reçu : %d",valeur_recue.donnee1);
-		GLCD_DrawString(1,1,(unsigned char*)chaine);
+			result = osMailGet(ID_BAL_ultrason, osWaitForever); // attente mail
+			recep = result.value.p; // on cible le pointeur...
+			valeur_recue = *recep ; // ...et la valeur pointée
+			osMailFree(ID_BAL_ultrason, recep); // libération mémoire allouée
 			
-		osDelay(50);
-	}		
+			sprintf(chaine_ultrason,"ULTRASON 0x002 : %d",valeur_recue.data_ultrason);
+			GLCD_DrawString(1,1,(unsigned char*)chaine_ultrason);
+				
+			osDelay(50);
+		}
 }
 
 void phares(void const *argument)
@@ -277,29 +254,23 @@ osThreadDef(ultrason,osPriorityNormal,1,0);
 osThreadDef(phares,osPriorityNormal,1,0);
 osThreadDef(gps,osPriorityNormal,1,0);
 
-/*
- * main: initialize and start the system
- */
-int main (void) {
-	
-	
-  
-	
+int main (void)
+{
+	// Init GLCD
 	GLCD_Initialize();
 	GLCD_ClearScreen();
-	GLCD_SetFont(&GLCD_Font_16x24);	// initialize CMSIS-RTOS
-	//GLCD_SetBackgroundColor (0x11);
+	GLCD_SetFont(&GLCD_Font_16x24);
 	
 	// Initialisation des 2 périphériques CAN
-	InitCan1();
-	InitCan2();
+	InitCan1(); // reception
+	InitCan2(); // emission
+	
 	osKernelInitialize ();
 	
-  // create 'thread' functions that start executing,
- // example: tid_name = osThreadCreate (osThread(name), NULL);
+	ID_BAL_ultrason = osMailCreate(osMailQ(NOM_BAL_ultrason), NULL);
 	id_CANthreadR = osThreadCreate (osThread(CANthreadR), NULL);
-	id_CANthreadT = osThreadCreate (osThread(CANthreadT), NULL);
-//	id_ultrason = osThreadCreate (osThread(ultrason), NULL);
+//	id_CANthreadT = osThreadCreate (osThread(CANthreadT), NULL);
+	id_ultrason = osThreadCreate (osThread(ultrason), NULL);
 //	id_phares = osThreadCreate (osThread(phares), NULL);
 //	id_gps = osThreadCreate (osThread(gps), NULL);
 
